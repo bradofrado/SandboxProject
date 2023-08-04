be1c96db88ac4246a4005edcf75d926286f4bccd
Braydon Jones <bjones@lucidchart.com>
2023-08-03 21:55:12 -0600

    Fix errors about the key in list

--- src/utils/components/base/side-panel.tsx
+++ src/utils/components/base/side-panel.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { type ParsedUrlQueryInput } from 'querystring';
+import React from 'react';
 import {NotifyLabel, type NotifyLabelProps} from '~/utils/components/base/notify-label';
 import {type IconComponent} from '~/utils/components/icons.tsx/icons';
 
@@ -22,21 +23,19 @@ export const SidePanel = ({className, items, children, onBodyClick}: SidePanelPr
     }
     return <div className="flex">
         <div className={`${className || ''}  z-40 w-64 h-screen transition-transform sm:translate-x-0`}>
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-700 border-r border-gray-200 dark:border-gray-700">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-700 border-r border-gray-200 dark:border-gray-600">
                     <ul className="space-y-2 font-medium">
                         {items.map((item, i) => {
                             const Icon = item.icon;
                             const selected = router.asPath.includes(item.href.pathname ?? queryToString(item.href.query));
                             const link = item.href;
-                            return <>
-                                <li key={i}>
-                                    <Link href={link} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group aria-selected:bg-gray-300 dark:aria-selected:bg-gray-500" aria-selected={selected}>
+                            return <li key={i}>
+                                    <Link href={link} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600 group aria-selected:bg-gray-300 dark:aria-selected:bg-gray-500" aria-selected={selected}>
                                         {Icon && <Icon className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white dark:group-aria-selected:text-white"/>}
                                         <span className="ml-3 flex-1">{item.label}</span>
                                         {item.notifyLabelItem && <NotifyLabel {...item.notifyLabelItem}/>}
                                     </Link>
                                 </li>
-                            </>
                         })}
                     </ul>
                 </div>
--- src/utils/components/navigation/navbar.tsx
+++ src/utils/components/navigation/navbar.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import {UserCircleIcon} from '~/utils/components/icons.tsx/icons';
 import Link from "next/link";
 import { useExpand } from "~/utils/hooks/useExpand";
+import React from "react";
 
 export interface NavItem {
     label: React.ReactNode,
@@ -25,18 +26,16 @@ export const NavbarComponent = ({items, title}: NavbarComponentProps) => {
                     onClick={() => onExpand()}>
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
                 <div className={`w-full md:block md:w-auto z-20 ${expandClass}`} id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        {items.map(item => {
+                        {items.map((item, i) => {
                             const selected = router.asPath.includes(item.link); 
-                            return <>
-                                <li>
+                            return <li key={i}>
                                     <Link href={item.link} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent aria-selected:text-blue-700 dark:aria-selected:text-blue-500" aria-current="page" aria-selected={selected}>{item.label}</Link>
                                 </li>
-                            </>
                         })}
                     </ul>
                 </div>
