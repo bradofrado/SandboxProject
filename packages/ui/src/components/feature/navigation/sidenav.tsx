1f1aa656c0dab4b19d71adf00a642babedacd72e
Braydon Jones <bradofrado@gmail.com>
2023-09-22 19:40:05 -0600

    Fix some things with the side panel

--- packages/ui/src/components/feature/navigation/sidenav.tsx
+++ packages/ui/src/components/feature/navigation/sidenav.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useExpand } from "../../../hooks/expand";
 import { HamburgerIcon } from "../../core/icons";
-import { SidePanelItems, SidePanel } from "../../core/side-panel";
+import { type SidePanelItems, SidePanel } from "../../core/side-panel";
 
 
 export type SideNavComponentProps = {
@@ -9,17 +9,17 @@ export type SideNavComponentProps = {
     className?: string,
 	path: string
 } & React.PropsWithChildren
-export const SideNavComponent = ({children, items, className, path}: SideNavComponentProps) => {
+export const SideNavComponent: React.FunctionComponent<SideNavComponentProps> = ({children, items, className, path}) => {
     const {onExpand, expandClass} = useExpand('translate-x-0', '-translate-x-full');
     
     return <>
         <button aria-controls="default-sidebar" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            onClick={() => {onExpand()}}>
+            onClick={() => {onExpand()}} type="button">
             <span className="sr-only">Open sidebar</span>
             <HamburgerIcon className="w-6 h-6"/>
         </button>
 
-        <SidePanel items={items} className={`${className || ''} ${expandClass}`} onBodyClick={() => {onExpand(false)}} path={path}>
+        <SidePanel className={`${className || ''} ${expandClass}`} items={items} onBodyClick={() => {onExpand(false)}} path={path}>
             {children}
         </SidePanel>
     </>
