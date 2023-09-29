ed1f1999a04609abb3cf853169d9bc0083dc8806
Braydon Jones <bradofrado@gmail.com>
2023-09-28 22:34:09 -0600

    Update the formatting to look a little cleaner

--- packages/ui/src/components/feature/chat/chat-box.tsx
+++ packages/ui/src/components/feature/chat/chat-box.tsx
@@ -1,6 +1,8 @@
 import { type User } from "model/src/user";
-
+import { displayDate, displayRelativeDate, groupBy } from "model/src/utils";
+import type { Message } from "model/src/message";
 import { MessageProvider } from "../../../services/message";
+import { Pill } from "../../core/pill";
 import { ChatMessage } from "./message";
 import { SendMessage } from "./send-message";
 
@@ -11,20 +13,40 @@ export interface ChatBoxProps {
 export const ChatBox: React.FunctionComponent<ChatBoxProps> = ({ user, className }) => {
   return (
     <MessageProvider>
-      {(messages) => (
-        <div className={`${className} flex flex-col max-h-screen dark:bg-[#282a2d] overflow-auto`}>
-          <div className="p-8 mb-15 flex flex-col gap-2 flex-1 justify-end">
-            {messages.map((message) => (
-              <ChatMessage
-                isUser={message.userId === user.id}
-                key={message.id}
-                message={message}
-              />
-            ))}
-          </div>
-          <SendMessage user={user} />
-        </div>
-      )}
+      {(messages) => {
+				const groupedByTime = groupBy(messages.map(message => ({...message, day: displayDate(message.date)})), 'day');
+				return (
+					<div className={`${className} flex flex-col max-h-screen dark:bg-[#282a2d] overflow-auto`}>
+						<div className="p-8 mb-15 flex flex-col gap-2 flex-1 justify-end">
+							{Object.entries(groupedByTime).map(([date, messages]) => (
+								<MessageDateSegment date={date} key={date} messages={messages} user={user}/>
+							))}
+						</div>
+						<SendMessage user={user} />
+					</div>
+				)
+			}}
     </MessageProvider>
   );
+}
+
+interface MessageDateSegmentProps {
+	messages: Message[],
+	date: string,
+	user: User
+}
+const MessageDateSegment: React.FunctionComponent<MessageDateSegmentProps> = ({messages, date, user}) => {
+	const dateReal = new Date(date);
+	return (
+		<div className="flex flex-col gap-2">
+			<div className="my-4">
+				<Pill className="w-fit mx-auto" mode='secondary'>{displayRelativeDate(dateReal)}</Pill>
+			</div>
+			{messages.map(message => <ChatMessage
+					isUser={message.userId === user.id}
+					key={message.id}
+					message={message}
+				/>)}
+		</div>
+	)
 }
--- packages/ui/src/services/message.tsx
+++ packages/ui/src/services/message.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState } from "react";
 import { type Message } from "model/src/message";
 import { type User } from "model/src/user";
+import { calculateDateDifference, day, hour, minute } from "model/src/utils";
 
 export type MessageContextType = [
   Message[],
@@ -14,9 +15,9 @@ export const MessageContext = createContext<MessageContextType>([
 export interface MessageProviderProps {
   children: (messages: Message[]) => React.ReactNode;
 }
-export const MessageProvider = ({ children }: MessageProviderProps) => {
+export const MessageProvider: React.FunctionComponent<MessageProviderProps> = ({ children }) => {
   const [_messages, setMessages] = useState<Message[]>(messages);
-  const sendMessage = (message: string, user: User) => {
+  const sendMessage = (message: string, user: User): void => {
     const copy = [..._messages];
     copy.push({
       id: "3",
@@ -24,12 +25,14 @@ export const MessageProvider = ({ children }: MessageProviderProps) => {
       name: user.name,
       text: message,
       avatar: user.image,
+			date: new Date()
     });
     setMessages(copy);
   };
+	const sorted = _messages.slice().sort((a, b) => a.date.getTime() - b.date.getTime());
   return (
-    <MessageContext.Provider value={[_messages, sendMessage]}>
-      {children(_messages)}
+    <MessageContext.Provider value={[sorted, sendMessage]}>
+      {children(sorted)}
     </MessageContext.Provider>
   );
 };
@@ -43,14 +46,32 @@ const messages: Message[] = [
     id: "0",
     userId: "0",
     name: "Bob",
-    text: "Hello there john",
+    text: "Not sure if you saw the notification under status but Maria missed her appointment yesterday at Spinal Rehab",
     avatar: "/braydon.jpeg",
+		date: calculateDateDifference(2 * day + 7 * hour)
   },
   {
     id: "1",
     userId: "1",
     name: "John",
-    text: "Hello there bob",
+    text: "Saw that. I will reach out to Maria.",
     avatar: "/braydon.jpeg",
+		date: calculateDateDifference(2 * day + 7 * hour - 33 * minute)
+  },
+	{
+    id: "2",
+    userId: "0",
+    name: "Bob",
+    text: "Thanks for reaching out to her! She was seen yesterday at spinal rehab.",
+    avatar: "/braydon.jpeg",
+		date: calculateDateDifference(day + 7 * hour)
+  },
+	{
+    id: "3",
+    userId: "1",
+    name: "John",
+    text: "Awesome! Let me know if there is anything else we can do to help!",
+    avatar: "/braydon.jpeg",
+		date: calculateDateDifference(day + 3 * hour)
   },
 ];
