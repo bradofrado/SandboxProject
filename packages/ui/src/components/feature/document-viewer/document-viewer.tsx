c7393ebaba8caa26510956eefe04e4cff2257117
Braydon Jones <bradofrado@gmail.com>
2023-10-04 09:28:54 -0600

    Add ability to click outside of a viewed document to close it

--- packages/ui/src/components/feature/document-viewer/document-viewer.tsx
+++ packages/ui/src/components/feature/document-viewer/document-viewer.tsx
@@ -1,5 +1,7 @@
+import { useRef } from "react";
 import { ModalPortal } from "../../core/modal";
 import { ClosableContent } from "../../core/closable-content";
+import { useClickOutside } from "../../../hooks/click-outside";
 import { PDFViewer } from "./viewers/pdf-viewer"
 import type {DocumentType, DocumentViewerComponent} from './types';
 import { ImageViewer } from "./viewers/img-viewer";
@@ -11,12 +13,19 @@ export interface DocumentViewerProps {
 	onClose: () => void
 }
 export const DocumentViewer: React.FunctionComponent<DocumentViewerProps> = ({src, type, show, onClose}) => {
+	const ref = useRef<HTMLDivElement>(null);
+	useClickOutside(ref, () => {
+		onClose();
+	})
+
 	if (show && (!src || !type)) throw new Error('Invalid src and type');
 	const Component = type && src ? documentComponents[type] : undefined;
 	return (
 		<ModalPortal show={show}>
 			<ClosableContent onClose={onClose}>
-				{type && src && Component ? <Component src={src}/> : null}
+				{type && src && Component ? <div className="w-fit mx-auto" ref={ref}>
+					<Component src={src} />
+				</div> : null}
 			</ClosableContent>
 		</ModalPortal>
 	)
